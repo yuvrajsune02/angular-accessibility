53341ff761455c49ffa339f8421ea2143d2bd63a
Yuvraj Sune <yuvrajsune02@gmail.com>
2025-05-25 19:03:40 +0530

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MiddleComponent } from './components/middle/middle.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    HeaderComponent,
+    SidebarComponent,
+    FooterComponent,
+    MiddleComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'angular-accessibility';
-}
+export class AppComponent {}
---
+++ src/app/components/middle/middle.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-middle',
+  imports: [],
+  templateUrl: './middle.component.html',
+  styleUrl: './middle.component.scss'
+})
+export class MiddleComponent {
+
+}
